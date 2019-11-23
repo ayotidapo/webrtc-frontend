69ffc3ba97f1256f62351e29001ec8d57a0c9bab
otidapson <oladapo.o@softcom.ng>
2019-11-23 16:48:23 +0100

    working local front

---
+++ build/precache-manifest.dc727f3c6458ab1c2d7aa3b0a4f8c144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969dde14b35cb74c0d50a8c848f8cf51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2a9430a34587e970986",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3973110e6ebcefed015",
+    "url": "/static/js/2.d0045e63.chunk.js"
+  },
+  {
+    "revision": "b2a9430a34587e970986",
+    "url": "/static/js/main.26011372.chunk.js"
+  },
+  {
+    "revision": "68515c8d84595ebbd0f8",
+    "url": "/static/js/runtime-main.d23901cf.js"
+  }
+]);
