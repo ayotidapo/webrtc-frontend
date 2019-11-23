6a1b15c670da0b728cfd05ec8cc45e3641de8e49
otidapson <oladapo.o@softcom.ng>
2019-11-23 12:09:58 +0100

    hello

---
+++ build/precache-manifest.62fb13426a8d6ad93305517d4968f925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e11a654fe0e0524a0c179c091ab7658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214c64cfa7092888e814",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a3973110e6ebcefed015",
+    "url": "/static/js/2.d0045e63.chunk.js"
+  },
+  {
+    "revision": "214c64cfa7092888e814",
+    "url": "/static/js/main.d1397c1f.chunk.js"
+  },
+  {
+    "revision": "68515c8d84595ebbd0f8",
+    "url": "/static/js/runtime-main.d23901cf.js"
+  }
+]);
