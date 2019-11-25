b4d760b6cb44a77c8f2bf8082507edcdcd5ad00e
otidapson <oladapo.o@softcom.ng>
2019-11-25 11:38:59 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 let socket;
 // connect to backend server..
-socket = window.io.connect("http://localhost:8080/", {
+socket = window.io.connect("https://cryptic-atoll-80533.herokuapp.com/", {
   transports: ["websocket"]
 });
 let connection;
@@ -48,7 +48,7 @@ function App() {
 
   const initSocketIO = () => {
     connection = initRTCMultiConnection();
-    connection.socketURL = "http://llocalhostt:8080/";
+    connection.socketURL = "https://cryptic-atoll-80533.herokuapp.com/";
 
     // this RTCMultiConnection object is used to connect with existing users
     connection.getExternalIceServers = false;
